95a09ff7420a61762c0da4d19ef826af3c1e528d
Reza Alipour <56475612+Reza-Alipour@users.noreply.github.com>
2023-12-04 23:04:54 +0330

    Change empty captions to a default caption

--- training/train_controlnet.py
+++ training/train_controlnet.py
@@ -663,7 +663,7 @@ def make_train_dataset(args, tokenizer, accelerator):
         cpl = caption_column if random.randint(0, 1) == 0 else 'captions'
         for caption in examples[cpl]:
             if random.random() < args.proportion_empty_prompts:
-                captions.append("High quality face image")
+                captions.append("High quality close-up portrait of a person's face")
             elif isinstance(caption, str):
                 captions.append(caption)
             elif isinstance(caption, (list, np.ndarray)):
